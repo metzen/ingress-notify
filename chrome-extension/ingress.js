f1b24cc8e3128c1c3f575a5fda1327c75e18bb58
Matt McDonald <metzen@gmail.com>
2012-12-04 00:31:49 -0500

    Use runtime.getBackgroundPage

--- chrome-extension/ingress.js
+++ chrome-extension/ingress.js
@@ -1,18 +1,22 @@
-var oauth = chrome.extension.getBackgroundPage().oauth;
-
 function PortalsCtrl($scope, $http) {
   $scope.portals = null;
   //$http.get('https://ingress-notify.appspot.com/portals').success(function(data) {
   //  $scope.portals = data;
   //});
   // TODO: Send OAuth token on requests with $http.
-  oauth.sendSignedRequest('https://ingress-notify.appspot.com/portals', function(resp, xhr) {
-    $scope.portals = JSON.parse(resp.substr(6))
-    $scope.$digest();
-  }, {'method': 'GET'});
+  chrome.runtime.getBackgroundPage(function(backgroundPage) {
+      backgroundPage.oauth.sendSignedRequest(
+        'https://ingress-notify.appspot.com/portals', function(resp, xhr) {
+          $scope.portals = JSON.parse(resp.substr(6));
+          $scope.$digest();
+        }, {'method': 'GET'});
+  });
 
   $scope.unwatch = function(portal) {
     $scope.portals.splice($scope.portals.indexOf(portal), 1);
-    $http.delete('https://ingress-notify.appspot.com/portals/' + portal.latE6 + ',' + portal.lngE6 + '-' + portal.name + '/watch');
+    portal.watched = false;
+    $http.put(
+        'https://ingress-notify.appspot.com/portals/' + portal.latE6 + ',' + portal.lngE6,
+        portal);
   };
 }
