c2f2edff46ba1b7830f13c95172b7937c95bf930
Matt McDonald <metzen@gmail.com>
2012-12-08 20:03:09 -0500

    Enable appstats

---
+++ server/appengine_config.py
@@ -0,0 +1,6 @@
+"""App Engine configuration module."""
+
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
