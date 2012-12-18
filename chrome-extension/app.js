9ad71e1250f523527f06d81ee6201cf967bb021b
Matt McDonald <metzen@gmail.com>
2012-12-18 14:01:27 -0500

    Fix for OAuth tokens on requests that contain query params

--- chrome-extension/app.js
+++ chrome-extension/app.js
@@ -1,8 +1,10 @@
 angular.module('ingress', [], function($provide) {
   $provide.decorator('$httpBackend', function($delegate) {
     return function(method, url, post, callback, headers, timeout, withCredentials, responseType) {
+      var urlParts = url.split('?');
       chrome.runtime.getBackgroundPage(function(backgroundPage) {
-        headers['Authorization'] = backgroundPage.oauth.getAuthorizationHeader(url, method);
+        headers['Authorization'] = backgroundPage.oauth.getAuthorizationHeader(
+            urlParts[0], method, urlParts[1]);
         $delegate(method, url, post, callback, headers, timeout, withCredentials, responseType);
       });
     };
